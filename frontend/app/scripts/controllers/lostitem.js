f57758f2cbb4144d7faf1c09274ff3f6bcd469b8
Sönke Erfkamp <soenke.erfkamp@tum.de>
2016-06-05 13:34:02 +0200

    main app design changes

--- frontend/app/scripts/controllers/lostitem.js
+++ frontend/app/scripts/controllers/lostitem.js
@@ -8,11 +8,32 @@
  * Controller of the lostcrowdfoundApp
  */
 angular.module('lostcrowdfoundApp')
-  .controller('LostitemCtrl', function ($scope, NgMap, itemsService) {
+  .controller('LostitemCtrl', function ($scope, NgMap, itemsService, ngToast) {
 
-  	NgMap.getMap().then(function(map) {
-      map = map;
-  	});
+    var map;
+    var vm = this;
+
+    NgMap.getMap().then(function(evtMap) {
+      map = evtMap;
+    });
+
+    vm.radius = 1000;
+    vm.lat = 48.138370;
+    vm.lon = 11.578553;
+
+    vm.area = function(event) {
+      var pos = this.getPosition();
+
+      vm.lon = pos.lng();
+      vm.lat = pos.lat();
+
+      ngToast.create({
+        className: 'success',
+        dismissOnClick: true,
+        content: 'Successfully added your item to our database!',
+      });
+    }
+
 
   	$scope.type = "";
   	$scope.brand = "";
