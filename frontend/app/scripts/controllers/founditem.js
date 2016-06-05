3b2b1023988b242b9d39e8f78ad2b1f22538c933
Sönke Erfkamp <soenke.erfkamp@tum.de>
2016-06-06 00:57:15 +0200

    style adjustments

--- frontend/app/scripts/controllers/founditem.js
+++ frontend/app/scripts/controllers/founditem.js
@@ -17,8 +17,41 @@ angular.module('lostcrowdfoundApp')
       map = evtMap;
   	});
 
-  	$scope.type = "";
-  	$scope.brand = "";
+    $scope.types = ["Smartphone"];
+    $scope.brands = ["Apple", "Samsung", "Microsoft"],
+    $scope.models = ["iPhone 5s", "iPhone 6", "iPhone 5c"];
+
+/*    $scope.brandSelectionVisible = true;
+    $scope.nameSelectionVisible = true;
+
+    $scope.type = "";
+
+    $scope.getBrands = function() {
+      switch(type) {
+        case "Smartphone":
+          return ["Apple, Samsung, Microsoft"];
+          break;
+        default:
+          $scope.brandSelectionVisible = false;
+          $scope.nameSelectionVisible = false;
+      }
+    }*/
+
+/*    $scope.getNames = function() {
+      switch(brandSelection) {
+        case "Apple":
+          $scope.names = ["iPhone 5s, iPhone 6, iPhone 5c"];
+          break;
+        case "Samsung":
+          $scope.names = ["Galaxy S5, Galaxy S6"];
+          break;
+        case "Microsoft":
+          $scope.names = ["Lumia 930,  Lumia 930 XL"];
+          break;
+      }
+    }
+    */
+
   	$scope.name = "";
   	$scope.email = "";
 
@@ -35,7 +68,7 @@ angular.module('lostcrowdfoundApp')
     $scope.addItem = addItem;
 
     function addItem () {
-  		itemsService.addItem($scope.type, $scope.brand, $scope.name, $scope.email, vm.lat, vm.lon);
+  		itemsService.addItem($scope.typeSelection, $scope.brandSelection, $scope.modelSelection, $scope.email, vm.lat, vm.lon);
       $location.path("/#");
       ngToast.create({
         className: 'success',
