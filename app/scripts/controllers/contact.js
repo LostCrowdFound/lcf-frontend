0234f3d1c4e7bcaed7cfc0d28e98afd8bddeac4d
Sönke Erfkamp <Snke Erfkamp>
2016-07-03 01:47:34 +0200

    add basic stuff, login required

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,10 +8,34 @@
  * Controller of the lostcrowdfoundApp
  */
 angular.module('lostcrowdfoundApp')
-  .controller('ContactCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ContactCtrl', function (currUser, $location, $window, ngToast) {
+    var vm = this;
+
+    vm.requestAd = function () {
+      if(currUser.loggedIn()) {
+        $location.path('/requestAd');
+      } else {
+        $window.localStorage.redirect = 'requestAd';
+        $location.path('/login');
+        ngToast.create({
+          className: 'danger',
+          dismissOnClick: true,
+          content: 'You need to log in first!',
+        });
+      }
+    };
+
+    vm.importItems = function () {
+      if(currUser.loggedIn()) {
+        $location.path('/importItems');
+      } else {
+        $window.localStorage.redirect = 'importItems';
+        $location.path('/login');
+        ngToast.create({
+          className: 'danger',
+          dismissOnClick: true,
+          content: 'You need to log in first!',
+        });
+      }
+    };
   });
